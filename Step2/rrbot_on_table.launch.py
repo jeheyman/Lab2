c022dd172efc73ca5fb104c3a538e333185996a8
Jack Elliot Heyman <jheyman@eceprog5.ecn.purdue.edu>
2026-02-17 22:11:43 -0500

    Stuck on task2

---
+++ Step2/rrbot_on_table.launch.py
@@ -0,0 +1,29 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command
+
+def generate_launch_description():
+    
+    return LaunchDescription([
+         Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': Command(['xacro ', 'table.urdf'])}]
+        ),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': Command(['xacro ', 'rrbot_on_table.urdf'])}]
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', 'rrbot.rviz']
+        )
+    ])
