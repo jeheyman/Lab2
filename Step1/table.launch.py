be1fa8456c8a94e7d6e5ea6709d43bae2ffd5033
Jack Heyman <jheyman@purdue.edu>
2026-02-18 21:03:13 -0500

    Created Step2 Task4 files

--- Step1/table.launch.py
+++ Step1/table.launch.py
@@ -17,12 +17,6 @@ def generate_launch_description():
             executable='rviz2',
             name='rviz2',
             output='screen',
-<<<<<<< HEAD
-            arguments=['-d', 'rrbot.rviz']
-        )
-    ])
-=======
             arguments=['-d', 'table.rviz']
         )
     ])
->>>>>>> 7da7082 (Built step2 task1)
